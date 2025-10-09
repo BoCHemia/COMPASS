3c5d692eeb59cdd16122d4a5df217d064ef2d6fb
Jasmin Hafner <jasmin.hafner@epfl.ch>
2025-10-09 18:02:36 +0200

    ADD: visualization of background space and mapping new data on top

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -11,6 +11,21 @@ input_folder = 'input'
 input_file = 'pfas_envipath'
 new_coordinates = load_coordinates(foldername=input_folder, filename=input_file)
 
-# plot - todo: not working yet
-# chemical_space_plot_grey(reference_coordinates)
+# load input coordinates, to be mapped to reference
+input_file_2 = 'pfas_market_inventory'
+new_coordinates_2 = load_coordinates(foldername=input_folder, filename=input_file_2)
+
+# plots
+# figure = chemical_space_plot(reference_coordinates, hue_column=None, color_map=None, hover_data=['INCHIKEY', 'SMILES'])
+# save_figure(figure, input_file)
+# quit()
+
+figure = chemical_space_plot_grey(reference_coordinates, hover_data=['INCHIKEY', 'SMILES'], opacity=0.5)
+figure = map_input_data(figure, new_coordinates_2, nametag=input_file_2,
+                        hover_name='PREFERRED_NAME', hover_data=['INCHIKEY', 'CAS', 'Synonyms'])
+# figure = map_input_data(figure, new_coordinates, nametag=input_file,
+#                         hover_name='SMILES', hover_data=['INCHIKEY'],
+#                         color='SIZE')
+figure.show()
+save_figure(figure, input_file)
 # chemical_space_plot(new_coordinates)
