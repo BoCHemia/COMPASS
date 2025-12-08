2db2a60c36933634abcfc624c5530bac4facb82e
Kerstin von Borries <kejbo@dtu.dk>
2025-12-08 13:56:02 +0100

    ADD: Saving offset from re-embedding to correct transformation with trained t-SNE.

--- scripts/transform.py
+++ scripts/transform.py
@@ -2,20 +2,23 @@ from modules.modeling import *
 from modules.preprocessing import *
 
 # load tSNE model object
-reference_file_name = "zeropm"
-model = load_model(reference_file_name, from_zip=False)
+reference_file_name = "coconut"
+model = load_model(reference_file_name, use_joblib=False, from_zip=False)
+offset = load_model_offset(reference_file_name) # add to app.py
 
 # load data to plot
-folder_name = "PFAS"
-file_name = 'pfas_nist' # enter here the name of the input data set to transform
+folder_name = "AgroTrak"
+file_name = 'zhang_2025_partial' # enter here the name of the input data set to transform
 
-new_df = load_input_file(file_name, folder_name=folder_name)
-new_fingerprints = preprocess_data(new_df)
-save_fingerprints(fingerprints=new_fingerprints, folder_name=folder_name, file_name=file_name,)
-# new_fingerprints = load_fingerprints(filename=new_data_name)
+# new_df = load_input_file(file_name, folder_name=folder_name)
+# new_fingerprints = preprocess_data(new_df)
+# save_fingerprints(fingerprints=new_fingerprints, folder_name=folder_name, file_name=file_name,)
+new_fingerprints = load_fingerprints(file_name=file_name, folder_name=folder_name)
 
 # transform
 coordinates = transform_target(model, new_fingerprints)
+coordinates = coordinates - offset.values # add to app.py
+
 save_coordinates(coordinates=coordinates,
                  folder_name=folder_name,
                  file_name=file_name,
